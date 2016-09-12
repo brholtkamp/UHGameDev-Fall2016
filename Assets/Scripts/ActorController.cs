6a1db1e66de87bb3d150d4871c4aedf47f909f78
Brian Holtkamp <brholtkamp@gmail.com>
2016-09-12 00:16:53 -0500

    Propagated improvements from lesson2 back to lesson1

--- Assets/Scripts/ActorController.cs
+++ Assets/Scripts/ActorController.cs
@@ -1,27 +1,42 @@
 ﻿using System.Linq;
+
 using UnityEngine;
 
 public class ActorController : MonoBehaviour {
+    /// <summary>
+    /// Speed of our actor's horizontal movement
+    /// </summary>
     public float Speed = 1.0f;
-    public float JumpHeight = 1.0f;
 
+    /// <summary>
+    /// Is this actor currently on the ground?
+    /// </summary>
     public bool IsGrounded { get; private set; }
 
+    /// <summary>
+    /// Velocity of this actor
+    /// </summary>
+    public Vector2 Velocity { get; set; }
+
+    /// <summary>
+    /// Is this actor facing to the right? (Default pose)
+    /// </summary>
     public bool FacingRight {
         get { return spriteRenderer.flipX == false; }
         set { spriteRenderer.flipX = !value; }
     }
 
-    private Vector2 velocity;
-
     private Rigidbody2D rigidBody;
-    private Animator animator;
     private SpriteRenderer spriteRenderer;
     private BoxCollider2D boxCollider;
 
+    /// <summary>
+    /// Tracker to make sure that we leave the ground
+    /// </summary>
+    private bool hasLeftGround;
+
     public void Awake() {
         rigidBody = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         boxCollider = GetComponent<BoxCollider2D>();
 
@@ -33,24 +48,36 @@ public class ActorController : MonoBehaviour {
         // Check our ground below us
         GroundCheck();
 
-        if (velocity != Vector2.zero) {
-            // Change the velocity of our actor
-            rigidBody.velocity = velocity;
+        if (Velocity != Vector2.zero) {
+            // Change the Velocity of our actor
+            rigidBody.velocity = Velocity;
+
+            // If we're supposed to be going up, and we're already grounded, make sure we leave the ground
+            if (Velocity.y > 0.0f && IsGrounded) {
+                IsGrounded = false;
+                hasLeftGround = false;
+            }
         }
 
-        // Clear our our velocity
-        velocity = Vector2.zero;
+        // Clear our our Velocity
+        Velocity = Vector2.zero;
     }
 
+    /// <summary>
+    /// Checks the ground and assigns IsGrounded
+    /// </summary>
     private void GroundCheck() {
         // Cast a box below us just a hair to see if there's any objects below us
-        var hits = Physics2D.BoxCastAll(boxCollider.bounds.center, boxCollider.bounds.size * 0.9f, 0.0f, Vector2.down, 0.1f);
+        var hits = Physics2D.BoxCastAll(boxCollider.bounds.center, new Vector2(boxCollider.bounds.size.x * 0.9f, boxCollider.bounds.size.y), 0.0f, Vector2.down, 0.1f);
 
-        // Check to see if any of the things we hit is in the Terrain layer
-        if (hits.Any(hit => hit.collider.gameObject.layer == LayerMask.NameToLayer("Terrain"))) {
+        // Check to see if any of the things we hit is in the Terrain layer and that we've already left the ground
+        if (hits.Any(hit => hit.collider.gameObject.layer == LayerMask.NameToLayer("Terrain")) && hasLeftGround) {
             IsGrounded = true;
         } else {
             IsGrounded = false;
+
+            // If we can't hit the ground anymore, that means we've successfully left the ground below us
+            hasLeftGround = true;
         }
     }
 
@@ -62,30 +89,20 @@ public class ActorController : MonoBehaviour {
         // If our direction is positive, we're moving to the right
         if (direction > 0.0f) {
             FacingRight = true;
-        // Otherwise, we're going left
-        // Note: we don't care about 0.0f, because it'd be unusual for our character to constantly face right
+            // Otherwise, we're going left
+            // Note: we don't care about 0.0f, because it'd be unusual for our character to constantly face right
         } else if (direction < 0.0f) {
             FacingRight = false;
         }
 
-        // Set our velocity to move on the next FixedUpdate tick
-        velocity = new Vector2(direction * Speed, rigidBody.velocity.y);
-    }
-
-    /// <summary>
-    /// Make our actor jump
-    /// </summary>
-    public void Jump() {
-        if (IsGrounded) {
-            // Set our new velocity to jump to our predesignated height
-            velocity = new Vector2(rigidBody.velocity.x, Mathf.Sqrt(-2.0f * JumpHeight * Physics2D.gravity.y));
-        }
+        // Set our Velocity to move on the next FixedUpdate tick
+        Velocity = new Vector2(direction * Speed, rigidBody.velocity.y);
     }
 
     /// <summary>
     /// Sets our colliders to the correct size
     /// </summary>
-    private void UpdateCollider() {
+    public void UpdateCollider() {
         boxCollider.size = spriteRenderer.bounds.size;
     }
 }
--- Assets/Scripts/MarioController.cs
+++ Assets/Scripts/MarioController.cs
@@ -1,27 +1,102 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(ActorController))]
+[RequireComponent(typeof(Jumping))]
+[RequireComponent(typeof(Animator))]
 public class MarioController : MonoBehaviour {
+    /// <summary>
+    /// Multiplier for the velocity of running
+    /// </summary>
     public float RunMultiplier = 1.5f;
 
+    /// <summary>
+    /// Amount of force to bounce off an enemy with
+    /// </summary>
+    public float BounceAmountWhenHittingEnemy = 6.0f;
+
+    /// <summary>
+    /// Are we currently Big Mario?
+    /// </summary>
+    public bool IsBigMario {
+        get { return animator.runtimeAnimatorController.name.Contains("Big"); }
+    }
+
+    /// <summary>
+    /// Is Mario currently invulnerable?
+    /// </summary>
+    public bool IsInvulnerable {
+        get { return currentInvulnerabilityTime > 0.0f; }
+    }
+
+    /// <summary>
+    /// Length of Mario's invulnerability after being hit
+    /// </summary>
+    public float InvulnerabilityDuration = 0.5f;
+
     private ActorController controller;
+    private Jumping jumping;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    /// <summary>
+    /// Tracker for the amount of invulnerability left
+    /// </summary>
+    private float currentInvulnerabilityTime;
+
+    /// <summary>
+    /// Tracker to make sure the player was running before jumping if they want to keep the horizontal momentum of running
+    /// </summary>
+    private bool wasRunningBeforeJump = false;
 
     public void Awake() {
         controller = GetComponent<ActorController>();
+        jumping = GetComponent<Jumping>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void Update() {
+        // Get the input from A/D of WASD or Left/Right from arrow keys
         var horizontalInput = Input.GetAxis("Horizontal");
+
+        // Check to see if there even is an input
         if (Mathf.Abs(horizontalInput) > 0.0f) {
-            if (Input.GetKey(KeyCode.LeftShift) && controller.IsGrounded) {
+            // Handle the case for running
+            if (Input.GetKey(KeyCode.LeftShift) && (controller.IsGrounded || wasRunningBeforeJump)) {
                 controller.Move(horizontalInput * RunMultiplier);
-            } else {
+                wasRunningBeforeJump = true;
+            }
+            // Handle the case for normal walking
+            else {
                 controller.Move(horizontalInput);
+                wasRunningBeforeJump = false;
             }
         }
-
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            controller.Jump();
+        // There was no input, so we should be idle
+        else if (controller.Velocity == Vector2.zero) {
+            // Idle
         }
+
+        // Make Jumpman jump!
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            jumping.Jump();
+        }
+    }
+
+    /// <summary>
+    /// Transform this Mario into little Mario
+    /// </summary>
+    public void TurnLittleMario() {
+        animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>("AnimationControllers/LittleMarioController");
+        controller.UpdateCollider();
+    }
+
+    /// <summary>
+    /// Transform this Mario into big Mario
+    /// </summary>
+    public void TurnBigMario() {
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f);
+        animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>("AnimationControllers/BigMarioController");
+        controller.UpdateCollider();
     }
 }
