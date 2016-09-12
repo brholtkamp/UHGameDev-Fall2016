f26826f055e6651a5b7b91f188dcaea86fc4ec98
Brian Holtkamp <brholtkamp@gmail.com>
2016-09-12 00:33:51 -0500

    Removed bounce method

--- Assets/Scripts/Jumping.cs
+++ Assets/Scripts/Jumping.cs
@@ -21,12 +21,4 @@ public class Jumping : MonoBehaviour {
             controller.Velocity = new Vector2(rigidBody.velocity.x, Mathf.Sqrt(-2.0f * JumpHeight * Physics2D.gravity.y));
         }
     }
-
-    /// <summary>
-    /// Make our actor bounce upwards
-    /// </summary>
-    /// <param name="amount">Amount of force to bounce by</param>
-    public void Bounce(float amount) {
-        controller.Velocity = new Vector2(controller.Velocity.x, amount);
-    }
 }
